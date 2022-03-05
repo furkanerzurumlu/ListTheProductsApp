baa92e12aefefa9ead1e902acff2729d8261bf9a
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-03-05 20:37:01 +0300

    Initial Commit

---
+++ DetayliTableViewKullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetayliTableViewKullanimi
+//
+//  Created by Furkan Erzurumlu on 5.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
